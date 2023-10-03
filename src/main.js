669512246278be527c7f06780d5a218c95294425
Shi Da <tanshida.53@gmail.com>
2023-10-04 00:12:37 +0800

    Added Bootstrap JS

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { apiKey,messagingSenderId,appId,measurementId } from "./config";
 
 // Bootstrap
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
