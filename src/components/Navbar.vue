d3e3daa32302568be7e70787a9036bada4ed8d58
Shi Da <tanshida.53@gmail.com>
2023-10-18 02:00:21 +0800

    Updated navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,46 +1,53 @@
 <script setup>
 import { ref } from 'vue';
 
-let isExpanded = ref(false);
-function handleToggle() {
-  isExpanded.value = !isExpanded.value
+let isNavExpanded = ref(false);
+let isUserExpanded = ref(false);
+
+function handleNavToggle() {
+  isNavExpanded.value = !isNavExpanded.value;
+}
+
+function handleUserToggle() {
+  isUserExpanded.value = !isUserExpanded.value;
 }
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-sm sticky-top bg-black">
+  <nav class="navbar navbar-expand-md sticky-top bg-black">
     <div class="container-fluid general__container px-0">
       <a class="navbar-brand" href="./home">
         <b>Property</b>Hub
       </a>
 
-      <button @click="handleToggle" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <img v-if="isExpanded" class="nav__close-icon" src="../assets/img/Navbar/close.png">
+      <button @click="handleNavToggle" class="navbar-toggler" type="button" data-bs-toggle="collapse"
+        data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <img v-if="isNavExpanded" class="nav__close-icon" src="../assets/img/Navbar/close.png">
         <span v-else class="material-symbols-outlined text-white fs-1">menu</span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav w-100 align-items-sm-center">
+        <ul class="navbar-nav w-100 align-items-md-center">
           <li class="nav-item">
             <a class="nav-link" href="./buy">Buy</a>
           </li>
 
-          <li class="nav-item mb-5 mb-sm-0">
+          <li class="nav-item mb-5 mb-md-0">
             <a class="nav-link" href="./sell">Sell</a>
           </li>
 
           <hr class="text-white-50">
 
-          <div class="d-sm-flex w-100 justify-content-end">
+          <div class="d-md-flex w-100 justify-content-end">
             <!-- <li class="nav-item">
               <a class="nav-link pe-0" href="#">Sign In</a>
             </li> -->
 
-            <li class="nav-item d-flex justify-content-between d-sm-none">
+            <li class="nav-item d-flex justify-content-between d-md-none">
               <a class="nav-link pe-0" href="#">
                 <img class="nav__user-img rounded-circle me-3 me-sm-1" src="https://source.unsplash.com/g0zwKn5vslI" />
-                <span class="text-white pe-0 me-3">Chason Jui</span>
+                <span class="text-white fw-bold pe-0 me-3">Chason Jui</span>
               </a>
 
               <a href="#">
@@ -48,13 +55,21 @@ function handleToggle() {
               </a>
             </li>
 
-            <li class="nav-item d-none d-sm-block">
-              <button class="btn p-0" type="button">
+            <li class="nav-item user__container d-none d-md-block">
+              <button @click="handleUserToggle" class="btn p-0" type="button">
                 <img class="nav__user-img rounded-circle" src="https://source.unsplash.com/g0zwKn5vslI" />
               </button>
-              <!-- <div>
-                <a href="#">User Profile</a>
-              </div> -->
+
+              <div v-if="isUserExpanded" class="user__collapse p-3">
+                <a class="nav-link ps-0 d-flex align-items-center" href="#">
+                  <img class="nav__user-img rounded-circle me-3" src="https://source.unsplash.com/g0zwKn5vslI" />
+                  <span class="text-black fw-bold pe-0 me-3">Chason Jui</span>
+                </a>
+
+                <a href="#">
+                  <img class="nav__logout-icon" src="../assets/img/Navbar/logout2.png" />
+                </a>
+              </div>
             </li>
           </div>
         </ul>
@@ -104,7 +119,24 @@ function handleToggle() {
   object-fit: cover;
 }
 
-@media (min-width: 576px) {
+.user__container {
+  position: relative;
+}
+
+.user__collapse {
+  position: absolute;  
+  top: 50px;
+  right: 0;
+  width: 250px;
+  background-color: #f0f0f0;
+  border-radius: 5px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+@media (min-width: 768px) {
   .nav__user-img {
     width: 40px;
     height: 40px;
