31961e7041565fb035fc4c45234999183cdf6d47
zhijieboop <zexter18518@gmail.com>
2023-10-18 20:25:31 +0800

    Update ForgotPassword.vue

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,55 +1,86 @@
+<script setup>
+import Footer from "../components/Footer.vue";
+import Navbar from "../components/Navbar.vue";
+</script>
 <template>
-    <div class="container">
-      <div class="reset-form">
-        <h2 class="text-center">Reset Password</h2>
-        <form action="resetsuccess.html">
-          <div class="form-group">
-            <!--Password-->
-            <label for="password">Password:</label>
-            <input
-              type="password"
-              class="form-control"
-              id="password"
-              placeholder="Enter your password"
-              required
-            />
-          </div>
-          <div class="form-group">
-            <!--Confirm Password-->
-            <label for="confirmpassword">Confirm Password:</label>
-            <input
-              type="password"
-              class="form-control"
-              id="confirm"
-              placeholder="Enter your password"
-              required
-            />
-          </div>
-          <!--Reset button-->
-          <div class="d-grid gap-2">
-            <button type="submit" class="btn btn-primary btn-block mt-3">
-              Reset
-            </button>
-          </div>
-          
-        </form>
-      </div>
-    </div>
+  <!-- Navbar -->
+  <Navbar></Navbar>
 
+  <div class="reset-container">
+    <!-- Login Image on left side -->
+    <div class="login-image">
+      <img
+        src="../assets/img/LoginView/apartmentvector.svg"
+        alt="Vector Image"
+      />
+    </div>
+    <div class="w-60">
+      <!-- Forgot Password Form on right -->
+      <div class="reset-form">
+        <h2 class="mb-4 fw-bold">Reset Password</h2>
+
+        <!-- Password -->
+        <div class="form-group mb-2">
+          <label for="password" class="py-2 fw-bold">Password:</label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            placeholder="Enter your password"
+            
+          />
+        </div>
+        <!-- Password -->
+        <div class="form-group">
+          <label for="confirmpassword" class="py-2 fw-bold">Confirm Password:</label>
+          <input
+            type="password"
+            class="form-control"
+            id="confirmpassword"
+            placeholder="Enter your password"
+            
+          />
+        </div>
+       
+        <!-- Login button -->
+        <div class="d-grid gap-2">
+          <button type="submit" class="btn btn-primary mt-3">
+            Reset
+          </button>
+        </div>
+
+        <p class="my-2 text-danger text-center">{{ errMsg }}</p>
+      </div>
+      <!-- Sign up link -->
+
+    </div>
+  </div>
+  <!-- Footer -->
+  <Footer></Footer>
 </template>
 
 <style scoped>
-body {
-  background-color: white;
+.reset-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  height: calc(100vh - 65px - 87px);
+  width: 90%;
+  margin: 0 auto;
+}
+
+.login-image {
+  width: 60%;
+  max-width: 700px;
 }
 
 .reset-form {
-  max-width: 400px;
-  margin: 0 auto;
-  margin-top: 100px;
-  background: white;
-  padding: 30px;
-  border-radius: 5px;
-  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+  min-width: 400px;
+}
+
+@media only screen and (max-width: 992px) {
+  .login-image {
+    display: none;
+  }
 }
 </style>
