e83034c3eabd73a2460db88adae6049b169b689d
zhijieneo <96569155+neozhijie@users.noreply.github.com>
2023-10-28 19:24:06 +0800

    Added Delete Listing

--- src/components/CreateListing/Listing.vue
+++ src/components/CreateListing/Listing.vue
@@ -1,43 +1,149 @@
 <script setup>
 import { ref } from "vue";
+import { doc, getFirestore, deleteDoc } from "firebase/firestore";
+import { getDownloadURL, getStorage, deleteObject, ref as storageRef } from "firebase/storage";
 let isFavorited = ref(false);
+
+const props = defineProps({
+  listingId: String,
+  address: String,
+  listedPrice: String,
+  bedrooms: String,
+  bathrooms: String,
+  floorSize: String,
+  favoriteCounts: Number,
+  imgPath: String
+});
+
+const db = getFirestore();
 
 function handleFavorite() {
   event.preventDefault();
   isFavorited.value = !isFavorited.value;
 }
+
+let imagePath = "";
+
+function handleDelete(listingId, imgPath) {
+  event.preventDefault();
+  document.getElementById('btn-close').click();
+
+  // Delete image
+  console.log(imgPath)
+  console.log(props.imgPath)
+  console.log(listingId)
+  const storage = getStorage();
+  const imageRef = storageRef(storage, props.imgPath);
+
+  deleteObject(imageRef).then(() => {
+    console.log("File deleted successfully");
+    // Delete listing from db
+    deleteDoc(doc(db, "listings", props.listingId)).then(() => {
+      console.log("Listing deleted successfully");
+    });
+  }).catch((error) => {
+    console.error("Whoops image not deleted:", error);
+  });
+  // // Delete listing from db
+  // deleteDoc(doc(db, "listings", props.listingId)).then(() => {
+  //   console.log("Listing deleted successfully");
+  //   const storage = getStorage();
+  //   const imageRef = storageRef(storage, props.imgPath);
+
+  //   // Delete image
+  //   deleteObject(imageRef).then(() => {
+  //     console.log("File deleted successfully");
+  //   }).catch((error) => {
+  //     console.error("Whoops image not deleted:", error);
+  //   });
+  // });
+}
+  // Display listing image
+  const img = ref(null);
+  if (props.imgPath) {
+    const storage = getStorage();
+    const imgRef = storageRef(storage, props.imgPath);
+    console.log(props.imgPath)
+    imagePath = props.imgPath;
+
+    getDownloadURL(imgRef)
+      .then(url => {
+        img.value.src = url;
+      })
+      .catch(err => console.log(err.message))
+  }
 </script>
 
 <template>
+  <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <!-- modal-header -->
+        <div class="modal-header">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-8 text-start">
+                <b class="modal-title fs-4" id="deleteModalLabel">Delete</b>
+              </div>
+              <div class="col-4 text-end">
+                <button type="button" id="btn-close" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- modal-body -->
+        <div class="modal-body">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="text-center">
+                <div class="text-start d-flex flex-column justify-content-between h-100">
+                  <div>
+                    <span>Are you sure you want to delete this listing?</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer justify-content-space-between">
+          <button type="button" class="btn cancel-btn" data-bs-dismiss="modal">Cancel</button>
+          <button type="button" @click="handleDelete(listingId, imgPath)" class="btn btn-primary">Delete</button>
+        </div>
+      </div>
+    </div>
+  </div>
   <div class="card">
     <div class="row g-0">
       <div class="col-4">
-        <img src="../../assets/img/Listings/hudson-graves-nOJagMqGCpA-unsplash.jpg"
+        <img ref="img"
           class="card__img img-fluid rounded-start">
       </div>
-
+      <button class="delete-btn" data-bs-toggle="modal" data-bs-target="#deleteModal">
+        <span class="material-symbols-outlined me-1">delete</span> Delete
+      </button>
+      
       <div class="col-8">
         <div class="card-body d-flex flex-column justify-content-center h-100 p-2 px-3  py-lg-2">
           <div class="d-flex justify-content-between align-items-center">
-            <h5 class="card-title fw-bold mb-0">220B Bedok Central</h5>
+            <h5 class="card-title fw-bold mb-0">{{ address }}</h5>
 
             <button @click="handleFavorite" class="card__favorite-btn"
               :class="{ 'card__favorite-btn-active': isFavorited }">
               <img class="card__favorite-icon me-1"
                 src="../../assets/img/Listings/favorite_FILL1_wght400_GRAD0_opsz24.png">
               <span class="card__favorite-qty text-body-tertiary fw-bold"
-                :class="{ 'card__favorite-qty--active': isFavorited }">10</span>
+                :class="{ 'card__favorite-qty--active': isFavorited }">{{ favoriteCounts }}</span>
             </button>
           </div>
 
-          <p class="card-text text-muted mb-0">$530,000</p>
+          <p class="card-text text-muted mb-0">${{ Number(listedPrice).toLocaleString() }}</p>
           <hr class="text-black-50 my-3 my-sm-4 my-lg-3">
 
           <div class="d-flex">
             <div class="me-2 me-lg-3">
               <div class="d-flex">
                 <span class="material-symbols-outlined me-1">bed</span>
-                <b class="card__value">3</b>
+                <b class="card__value">{{ bedrooms }}</b>
               </div>
               <div class="text-muted card__property">Bedrooms</div>
             </div>
@@ -45,7 +151,7 @@ function handleFavorite() {
             <div class="me-2 me-lg-3">
               <div class="d-flex">
                 <span class="material-symbols-outlined me-1">bathtub</span>
-                <b class="card__value">2</b>
+                <b class="card__value">{{ bathrooms }}</b>
               </div>
               <div class="text-muted card__property">Bathrooms</div>
             </div>
@@ -53,7 +159,7 @@ function handleFavorite() {
             <div>
               <div class="d-flex">
                 <span class="material-symbols-outlined me-1">crop_square</span>
-                <b class="card__value">984 <span class="card__unit">sqft</span></b>
+                <b class="card__value">{{ floorSize }} <span class="card__unit">sqft</span></b>
               </div>
               <div class="text-muted card__property">Living Area</div>
             </div>
@@ -76,6 +182,32 @@ function handleFavorite() {
 
 .card-title {
   font-size: 12px;
+}
+
+.cancel-btn:hover {
+  background-color: #f0f0f0;
+}
+
+.delete-btn {
+  position: absolute;
+  width: fit-content;
+  right: 10px;
+  bottom: 10px;
+  display: flex;
+  padding: 2px 10px;
+  align-items: center;
+  border: 0;
+  background-color: unset;
+  visibility: hidden;
+}
+
+.delete-btn:hover {
+  background-color: #f0f0f0;
+  border-radius: 8px;
+}
+
+.card:hover .delete-btn {
+  visibility: visible;
 }
 
 .card__favorite-btn {
--- src/views/CreateListingPage.vue
+++ src/views/CreateListingPage.vue
@@ -3,23 +3,63 @@ import Footer from "../components/Footer.vue";
 import Navbar from "../components/Navbar.vue";
 import Listing from "../components/CreateListing/Listing.vue";
 import { useRouter } from "vue-router";
+import { ref } from "vue";
+import { getAuth } from "firebase/auth";
+import { doc, getFirestore, updateDoc, collection, getDoc, arrayUnion, onSnapshot, getDocs } from "firebase/firestore";
+import { query, where } from "firebase/firestore";
 const router = useRouter();
 const createlisting = () => {
   router.push("/createlisting");
 };
+
+const db = getFirestore();
+
+// Checks if user is logged in
+const auth = getAuth();
+const userId = ref(null);
+const isLoggedIn = ref(false);
+console.log(auth.currentUser)
+
+const listingsColRef = collection(db, "listings");
+const currentuserListingsQuery = query(listingsColRef, where("userEmail", "==", "tester1@wad.com"));
+// const q = query(listingRef, where("userEmail", "==", auth.currentUser.email));
+
+let userListings = ref([]);
+function displayListings(query, listings) {
+  onSnapshot(query, snapshot => {
+    listings.value = [];
+    snapshot.docs.forEach(listing => {
+      listings.value.push([listing.id, listing.data()]);
+    })
+  })
+}
+
+displayListings(currentuserListingsQuery, userListings);
+
+onSnapshot(listingsColRef, snapshot => {
+  displayListings(currentuserListingsQuery, userListings);
+});
 </script>
 
 <template>
     <Navbar/>
     <div class="container mx-auto my-5">
         <div class="d-flex justify-content-between">
-            <h2 class="mb-4 fw-bold">Listings</h2>
+            <h2 class="mb-4 fw-bold">My Listings</h2>
             <button @click="createlisting" type="button" class="mb-4 btn btn-primary createbtn">Create Listing</button>
         </div>
-        <Listing></Listing>
-        <Listing></Listing>
-        <Listing></Listing>
-        <Listing></Listing>
+        <div v-for="listing in userListings" :key="listing[0]">
+        <Listing :listingId="listing[0]"
+                 :address="listing[1].address"
+                 :listedPrice="listing[1].listedPrice"
+                 :bedrooms="listing[1].bedrooms"
+                 :bathrooms="listing[1].bathrooms"
+                 :floorSize="listing[1].floorSize"
+                 :favoriteCounts="listing[1].favoriteCounts"
+                 :isFavorited="listing[1].isFavorited"
+                 :imgPath="listing[1].imgPath"
+        ></Listing>
+        </div>
     </div>
     <Footer></Footer>
 </template>
