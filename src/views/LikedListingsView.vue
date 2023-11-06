c93f7665bee67be3103f454f3427f5c0b34015f6
Shi Da <tanshida.53@gmail.com>
2023-11-06 22:41:24 +0800

    Updated liked listings to reflect real time change (favorites)

--- src/views/LikedListingsView.vue
+++ src/views/LikedListingsView.vue
@@ -4,18 +4,19 @@ import Navbar from "../components/Navbar.vue";
 import Listing from "../components/BuyView/Listing.vue";
 import { ref } from "vue";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { getDoc, doc, getFirestore} from "firebase/firestore";
+import { getDoc, doc, getFirestore, collection, onSnapshot } from "firebase/firestore";
 import { getCurrentUser } from "../firebase/index.js";
 
 const db = getFirestore();
 
 // Checks if user is logged in
 const auth = getAuth();
+const userId = ref("");
 getCurrentUser()
   .then(user => {
     if (user) {
-      console.log(user);
-      getFavourites(user.uid)
+      // console.log(user);
+      userId.value = user.uid;
     } else {
       console.log("No user is currently logged in.");
     }
@@ -31,8 +32,9 @@ async function getFavourites(uid) {
   const docSnap = await getDoc(docRef);
 
   if (docSnap.exists()) {
-    console.log("Document data:", docSnap.data());
-    console.log(docSnap.data().favoritedListings);
+    // console.log("Document data:", docSnap.data());
+    // console.log(docSnap.data().favoritedListings);
+    favouriteListings.value = [];
     for (const favourite of docSnap.data().favoritedListings) {
         const listingRef = doc(db, "listings", favourite);
         const listingSnap = await getDoc(listingRef);
@@ -45,12 +47,18 @@ async function getFavourites(uid) {
   }
 }
 
+const customersColRef = collection(db, "customers");
 
+onSnapshot(customersColRef, () => {
+  if (userId.value) {
+    getFavourites(userId.value);
+  }
+})
 </script>
 
 <template>
     <Navbar/>
-    <div class="container mx-auto my-5">
+    <div class="container mx-auto py-5" :class="{ 'container--height' : favouriteListings && favouriteListings.length < 3 }">
         <h2 class="mb-4 fw-bold fs-3">Liked Listings</h2>
         <div v-for="listing in favouriteListings" :key="listing[0]">
             <Listing
@@ -75,6 +83,10 @@ async function getFavourites(uid) {
 .container{
     width: 90%;
     max-width: 600px;
+}
+
+.container--height {
+  height: calc(100vh - 65px - 84px);
 }
 </style>
 
